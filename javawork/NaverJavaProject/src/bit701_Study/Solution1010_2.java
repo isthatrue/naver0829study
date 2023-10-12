0929027913847c3761effd93a48a06ff781d9e74
jongbin <garon6484@gmail.com>
2023-10-12 20:46:43 +0900

    20231012_onevent

--- javawork/NaverJavaProject/src/bit701_Study/Solution1010_2.java
+++ javawork/NaverJavaProject/src/bit701_Study/Solution1010_2.java
@@ -1,21 +1,22 @@
 package bit701_Study;
 
 public class Solution1010_2 {
-
+	public int solution(int[] common) {
+        int answer = 0;
+        if ((common[1] - common[0]) == (common[2] - common[1])) 
+        	answer = (common[common.length-1]) + (common[2] - common[1]);
+		
+		else if ((common[1] / common[0]) == (common[2] / common[1])) { 
+	        answer = (common[common.length-1]) * (common[2] / common[1]);
+		}
+        return answer;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int townNumber = 0; 
-		int count = 0;
-		int n = 73;
-		while(n > count) {
-			townNumber++;
-			if(townNumber % 3 == 0 || Integer.toString(townNumber).contains("3"))
-				continue;
-			
-			count++;
-		}	
-			System.out.println(townNumber);
+		Solution1010_2 soul = new Solution1010_2();
+		int[] arr = {15, 30, 60, 120};
+		
+		System.out.println(soul.solution(arr));
 	}
-	
 
 }
