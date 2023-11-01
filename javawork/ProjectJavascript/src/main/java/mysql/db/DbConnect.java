df1ea90acd75409aa77465713bc544e26321dedd
jongbin <garon6484@gmail.com>
2023-11-01 18:01:24 +0900

    20231101_ajax_dataread, datasend

--- javawork/ProjectJavascript/src/main/java/mysql/db/DbConnect.java
+++ javawork/ProjectJavascript/src/main/java/mysql/db/DbConnect.java
@@ -12,7 +12,11 @@ import myshop.data.MyShopDto;
 
 public class DbConnect {
 	static final String MYSQL_DRIVER="com.mysql.cj.jdbc.Driver";
-	static final String MYSQL_URL="jdbc:mysql://localhost:3306/bit701?serverTimezone=Asia/Seoul";
+	// local mysql url
+	// static final String MYSQL_URL="jdbc:mysql://localhost:3306/bit701?serverTimezone=Asia/Seoul";
+	
+	// 원격 mysql url
+	static final String MYSQL_URL="jdbc:mysql://db-jbnml-kr.vpc-pub-cdb.ntruss.com:3306/studydb?serverTimezone=Asia/Seoul";
 	
 	public DbConnect() {
 		// TODO Auto-generated constructor stub
@@ -26,7 +30,11 @@ public class DbConnect {
 	public Connection getConnection() {
 		Connection conn = null;
 		try {
-			conn = DriverManager.getConnection(MYSQL_URL, "root", "1234");
+			// local 연결
+			// conn = DriverManager.getConnection(MYSQL_URL, "root", "1234");
+			
+			// 원격 mysql 연결
+			conn = DriverManager.getConnection(MYSQL_URL, "study", "bitcamp123!@#");
 		} catch (SQLException e) {
 			System.out.println("Mysql Local 연결실패 : " + e.getMessage());
 		}
