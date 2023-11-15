66415ef0a7bfd4854e2285671877d111ff411576
jongbin <garon6484@gmail.com>
2023-11-15 21:48:49 +0900

    20231115_spring_mysql

--- springwork/SpringMysqlBoardEx7/src/main/java/board/data/BoardDao.java
+++ springwork/SpringMysqlBoardEx7/src/main/java/board/data/BoardDao.java
@@ -1,5 +1,7 @@
 package board.data;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -14,5 +16,30 @@ public class BoardDao {
 	// 전체 개수 반환하는 메소드
 	public int getTotalCount() {
 		return session.selectOne(nameSpace + "totalCountOfBoard");
+	}
+	
+	public void insertBoard(BoardDto dto) {
+		session.insert(nameSpace + "insertBoard", dto);
+	}
+	
+	public List<BoardDto> getAllDatas() {
+		return session.selectList(nameSpace + "selectAllBoard");
+	}
+	
+	public void updateReadCount(int num) {
+		session.update(nameSpace + "updateReadCount", num);
+	}
+	
+	public BoardDto getData(int num) {
+		return session.selectOne(nameSpace + "selectOneData", num);
+	}
+	
+	public void deleteBoard(int num) {
+    session.delete(nameSpace+"deleteBoard",num);
+	}
+	
+	public void updateBoard(BoardDto dto) {
+		session.update(nameSpace + "updateBoard", dto);
+		
 	}
 }
