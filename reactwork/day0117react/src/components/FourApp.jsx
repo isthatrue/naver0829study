c26986c73fb9c3a9d0cda37dfc2a3e9dd6b156de
jongbin <garon6484@gmail.com>
2024-01-18 17:33:45 +0900

    [feat] #React [240118] - 방명록 페이지 실습 및 페이징 처리, React 환경 변수 (.env) 파일 사용

--- reactwork/day0117react/src/components/FourApp.jsx
+++ reactwork/day0117react/src/components/FourApp.jsx
@@ -1,13 +1,53 @@
 import { Alert } from "@mui/material";
 import FourGuestForm from "./FourGuestForm";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import FourGuestRowItem from "./FourGuestRowItem";
 
 const FourApp = () => {
+  const [guestList, setGuestList] = useState([]);
+
+  // 저장 함수
+  const onGuestSave = (data) => {
+    // data에는 닉네임과 글이 들어있음
+    axios.post("/guest/insert", data).then((res) => {
+      // 추가 성공 후 목록 다시 출력
+      list();
+    });
+  };
+
+  // 목록 출력하는 함수
+  const list = () => {
+    axios.get("/guest/list").then((res) => {
+      setGuestList(res.data);
+    });
+  };
+
+  // 처음 시작 시 목록 출력
+  useEffect(() => {
+    list();
+  }, []); // 처음 한번만 호출
+
+  const onDelete = (gnum) => {
+    axios.delete("/guest/delete?gnum=" + gnum).then((res) => {
+      // 삭제 후 목록 다시 출력
+      list();
+    });
+  };
+
   return (
     <div>
       <Alert severity="error" variant="filled">
-        <h3>FourApp-guest</h3>
+        <h3>FourApp-guestDB</h3>
       </Alert>
-      <FourGuestForm />
+      <FourGuestForm onSave={onGuestSave} />
+      <hr />
+      <h6>
+        <b>총 {guestList.length}개의 방명록 글이 있습니다.</b>
+      </h6>
+      {guestList.map((item, idx) => (
+        <FourGuestRowItem key={idx} item={item} onDelete={onDelete} />
+      ))}
     </div>
   );
 };
--- reactwork/day0117react/src/components/FourGuestForm.jsx
+++ reactwork/day0117react/src/components/FourGuestForm.jsx
@@ -1,10 +1,17 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
+import { Button } from "@mui/material";
 
-function FourGuestForm(props) {
+function FourGuestForm({ onSave }) {
   const [photo, setPhoto] = useState("");
   const [nickname, setNickname] = useState("");
-  const [content, setContent] = useState("");
+  // const [content, setContent] = useState("");
+
+  // 많은 내용을 입력하는 textarea는 state 변수로 줄 경우 입력 시 마다 랜더링이 일어나므로
+  // 문제가 발생하기도 한다.
+  // 그래서 많은 내용을 입력하는 경우는 ref 변수로 대체하는 게 좋다.
+
+  const contentRef = useRef("");
 
   const imageUrl =
     "https://kr.object.ncloudstorage.com/bitcamp.ncp-701/bootmyshop/";
@@ -24,11 +31,60 @@ function FourGuestForm(props) {
     });
   };
 
+  // 추가 버튼 이벤트
+  const addButtonEvent = () => {
+    let content = contentRef.current.value;
+    if (nickname.length === 0) {
+      alert("닉네임을 입력해주세요.");
+      return;
+    }
+    if (content.length === 0) {
+      alert("내용을 입력해주세요.");
+      return;
+    }
+
+    // 부모 컴포넌트의 함수로 입력 데이터 보내기
+    onSave({ nickname, content });
+
+    // 입력 값 초기화
+    setNickname("");
+    contentRef.current.value = "";
+    setPhoto("");
+  };
+
   return (
-    <div style={{ width: "400px" }}>
-      <input type="file" className="form-control" onChange={onUploadEvent} />
-      <img alt="" src={imageUrl + photo} width={130} />
-      <b>{photo}</b>
+    <div>
+      <div style={{ width: "400px" }}>
+        <input type="file" className="form-control" onChange={onUploadEvent} />
+        <img alt="" src={imageUrl + photo} width={130} />
+        <b>{photo}</b>
+      </div>
+      <div className="input-group" style={{ width: "400px" }}>
+        <input
+          type="text"
+          placeholder="닉네임"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+          className="form-control"
+        />
+        <Button color="info" variant="contained" onClick={addButtonEvent}>
+          추가
+        </Button>
+      </div>
+      {/* <textarea
+        className="form-control"
+        onChange={(e) => setContent(e.target.value)}
+      >
+        {content}
+      </textarea> */}
+      <textarea
+        className="form-control"
+        onChange={(e) => {
+          contentRef.current.value = e.target.value;
+        }}
+        ref={contentRef}
+        placeholder="방명록 내용 입력"
+      ></textarea>
     </div>
   );
 }
